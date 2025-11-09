205cd7ad9527947c5159c5e5fb6388969aec7ae7
Gage <aafire@firemail.cc>
2025-11-09 09:16:30 -0600

    Added a placeholder for the GUI varient

---
+++ src/gui-app/Main.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator extends JFrame implements ActionListener {
+    private JTextField display;
+    private double num1, num2, result;
+    private char operator;
+
+    public void main() {
+        setTitle("Jcalc");
+        setSize(300, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        display = new JTextField();
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.BOLD, 24));
+        add(display, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4, 5, 5));
+
+        String[] buttons = {
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", "C", "=", "+"
+        };
+
+        for (String text : buttons) {
+            JButton button = new JButton(text);
+            button.setFont(new Font("Arial", Font.BOLD, 20));
+            button.addActionListener(this);
+            panel.add(button);
+        }
+
+        add(panel, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String input = e.getActionCommand();
+
+        if (input.charAt(0) >= '0' && input.charAt(0) <= '9') {
+            display.setText(display.getText() + input);
+        } else if (input.charAt(0) == 'C') {
+            display.setText("");
+            num1 = num2 = result = 0;
+        } else if (input.charAt(0) == '=') {
+            num2 = Double.parseDouble(display.getText());
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/': result = num2 != 0 ? num1 / num2 : 0; break;
+            }
+            display.setText(String.valueOf(result));
+        } else {
+            num1 = Double.parseDouble(display.getText());
+            operator = input.charAt(0);
+            display.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
